1e3d6a223947dc2dd661c21bdff0a65dcbecf2e0
GordiyKrizanovskiy <gordiy.krizanovskiy@knu.ua>
2024-04-12 17:19:19 +0300

    Add function to read product data from CSV file

--- python.py
+++ python.py
@@ -1,3 +1,17 @@
 import datetime
 import csv
 from collections import defaultdict
+
+def read_product_data(filename, product_name):
+    # Read data from the file and store information about prices by date for the specified product
+    product_prices = defaultdict(list)
+    with open(filename, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            name, date_str, price = row
+            if name == product_name:
+                date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+                price = float(price)
+                product_prices[date].append(price)
+    
+    return product_prices
